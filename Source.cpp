349e995da4786fc812aaefa68dd92d3a4b9c4ebe
AndrewLukin <andreylukin3010@mail.ru>
2016-10-06 07:16:07 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+//Определить значение y при вводимом x для уравнения y = sqrt(x^2 - 2*x-7) / ((x-15)*(x^3 - 1)).
+void main()
+{
+	using namespace std;
+	setlocale(LC_ALL, "rus");
+	int x;
+	double y;
+	cout << "Введите х=";
+	cin >> x;
+	if ((x >= -2) && (x <= 4) || (x == 15))
+		printf_s("Введённый х не принадлежит ОДЗ!\n");
+	else
+	{
+		y = sqrt(pow(x, 2) - 2*x-7) / ((x-15)*(pow(x, 3) - 1));
+		printf("\ny=%f",y);
+	}
+	system("pause");
+}
