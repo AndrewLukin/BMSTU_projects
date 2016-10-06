e3570e0d28e767a1c2ef0ba6260f9c1107b92a69
AndrewLukin <andreylukin3010@mail.ru>
2016-10-06 07:17:51 +0300

    Update LAB_1_1.cpp

--- LAB_1_1.cpp
+++ LAB_1_1.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
-//Îïðåäåëèòü çíà÷åíèå y ïðè ââîäèìîì x äëÿ óðàâíåíèÿ y = sqrt(x^2 - 2*x-7) / ((x-15)*(x^3 - 1)).
+//Определить значение y при вводимом x для уравнения y = sqrt(x^2 - 2*x-7) / ((x-15)*(x^3 - 1)).
 void main()
 {
 	using namespace std;
 	setlocale(LC_ALL, "rus");
 	int x;
 	double y;
-	cout << "Ââåäèòå õ=";
+	cout << "Введите õ=";
 	cin >> x;
 	if ((x >= -2) && (x <= 4) || (x == 15))
-		printf_s("Ââåä¸ííûé õ íå ïðèíàäëåæèò ÎÄÇ!\n");
+		printf_s("Введённый х не принадлежит ОДЗ!\n");
 	else
 	{
 		y = sqrt(pow(x, 2) - 2*x-7) / ((x-15)*(pow(x, 3) - 1));
